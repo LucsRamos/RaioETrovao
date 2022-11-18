a2f3a66f0c149b8c7fb6ce755c29bd6a025e4198
Lucas Ramos <lucasramos.lr260@gmail.com>
2022-11-18 20:47:49 -0300

    Adiciona a solução do exercicio

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Raio e Travão\n");
+Console.WriteLine($"trovão a {30} segundos ==> raio caiu a {DistanciaDoRaioKm(30):N5} quilômetros");
+Console.WriteLine($"trovão a {40} segundos ==> raio caiu a {DistanciaDoRaioKm(40):N5} quilômetros");
+double DistanciaDoRaioKm(double segundos)
+{
+    const double VelocidadeDoSomKmS = 340.29 / 1000;
+    return segundos * VelocidadeDoSomKmS;
+}
